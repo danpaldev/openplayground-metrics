d9435cb104465b9fada28f4f6b15017ff6cf4bb4
danpaldev <contact@palacios.cloud>
2023-06-04 21:58:09 -0600

    feat: added informative accordion

--- src/lib/components/ModelsMetricsTable.svelte
+++ src/lib/components/ModelsMetricsTable.svelte
@@ -5,6 +5,7 @@
 	import { timestampsStore } from '$lib/stores/timestamps';
 	import { calculateAveragesByModel, epochToFormattedDate, type MetricAverages } from '$lib/utils';
 	import { LEGENDS } from '$lib/constants';
+	import { CodeSnippet, Accordion, AccordionItem } from 'carbon-components-svelte';
 
 	let averagesResults: Record<string, MetricAverages>;
 	let tableLegend = LEGENDS.DEFAULT_TABLE_LEGEND;
@@ -55,6 +56,36 @@
 			};
 		})}
 	/>
+	<div class="info-container">
+		<Accordion size="xl">
+			<AccordionItem title="How do we get and calculate the data?">
+				All the data in the site is collected by performing a completion request for each of the
+				listed models every five minutes. The next prompt is always used for all the models:
+				<br /><br />
+				<em
+					>"Generate a Django application with Authentication, JWT, Tests, DB support. Show
+					docker-compose for python and postgres. Show the complete code for every file!""</em
+				> <br /><br />
+				By requesting a "lengthy task", we ensure that the models generate the maximum amount of tokens
+				possible in order to obtain more precise calculations. In addition, we use the same parameters
+				for every request to allow fair comparisons between each one. These are the parameters:
+				<br /><br />
+				<br />
+				<CodeSnippet
+					type="multi"
+					code={`"temperature": 0,\n"maximumLength": 525,\n"topP": 1,\n"topK": 1`}
+				/>
+				<br /><br />
+				In order to get the <strong>completion response time</strong>, we calculate the time between
+				the start of the request and the moment when the model reaches its 525 token limit. During
+				this period, we also note how long it takes for the first token of the actual answer to
+				arrive (<strong>First Byte Token</strong>).<br /><br /> And finally, after the request is
+				done, we count the number of tokens received during the whole completion, and divide the
+				number by the amount of seconds that the completion took to finalize (completion response
+				time). This allows us to get the average number of <strong>Tokens per Second</strong>.
+			</AccordionItem>
+		</Accordion>
+	</div>
 </div>
 
 <style>
@@ -65,5 +96,9 @@
 
 	p {
 		text-align: center;
+	}
+
+	.info-container {
+		margin-top: 3rem;
 	}
 </style>
